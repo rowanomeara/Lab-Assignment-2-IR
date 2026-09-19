bae3ba12562a0dcc4ad6a3fe6fa55a8b2276c573
rowanomeara <rowan.omeara@student.uts.edu.au>
2026-09-19 17:33:45 +1000

    Added objects into main

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ class AutomatedCafe:
         self.env = swift.Swift()
         self.env.launch(realtime=True)
         
-        #self.barista = ABB_IRB120()
+        self.barista = ABB_IRB120()
+
         #self.baker = Jonas?
         #self.dispatcher = Lachlan?
         
@@ -26,15 +27,28 @@ class AutomatedCafe:
         """
         Add all robots, tables, and props into the Swift environment.
         """
-
-        if self.barista.base_link_mesh is not None:
-            self.env.add(self.barista.base_link_mesh)
-            
-        self.env.add(self.barista)
         
-        # Placeholder: Eventually we will add tables, coffee machines, etc. here.
-        # table = SE3(0, 0, 0)
-        # self.env.add(table)
+        #Barista Robot
+        self.barista.base = SE3(0, 0, 0.97)
+        self.barista.base_link_mesh.T = SE3(0, 0, 0.97)
+        self.env.add(self.barista.base_link_mesh)
+        self.env.add(self.barista)
+
+        import os
+        base_path = os.path.dirname(os.path.abspath(__file__))
+
+        #Counter Top
+        counter_path = os.path.join(base_path, "Workcell meshes", "Counter", "counter.stl")
+        self.counter = Mesh(counter_path, color=[0.8, 0.8, 0.8, 1.0])
+        self.counter.T = SE3(0.5, 0, 0) 
+        self.env.add(self.counter)
+
+        #Coffee Machine
+        machine_path = os.path.join(base_path, "Workcell meshes", "Coffee Machine", "machine.stl")
+        self.coffee_machine = Mesh(machine_path, color=[0.2, 0.2, 0.2, 1.0])
+        self.coffee_machine.T = SE3(0.3, 0.6, 0.97)*SE3.Rx(pi/2) 
+        self.env.add(self.coffee_machine)
+
 
     def run(self):
         """
